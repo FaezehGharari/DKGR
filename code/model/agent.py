f044261000a77ef4abd6cdd69c5cb3e97d4cb602
FaezehGharari <fgharari@gmail.com>
2024-12-14 17:34:47 +0330

    Update agent.py

--- code/model/agent.py
+++ code/model/agent.py
@@ -1,36 +1,48 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-import torch.nn.utils as utils
+
 
 
 class Policy_step(nn.Module):
     def __init__(self, m, embedding_size, hidden_size):
         super(Policy_step, self).__init__()
 
-        self.batch_norm = nn.BatchNorm1d(m * hidden_size)
-        self.lstm_cell = nn.LSTMCell(input_size=2 * m * embedding_size, hidden_size=2 * m * hidden_size)
-        self.l1 = nn.Linear(m * embedding_size, 2 * m * embedding_size)
+        self.embedding_size = embedding_size
+        self.m=m
+        self.hidden_size = hidden_size
+        self.transformer = nn.Transformer(d_model = m * embedding_size,
+                                          num_encoder_layers=1,
+                                          num_decoder_layers=1, nhead=1,
+                                          dim_feedforward= m * self.hidden_size,
+                                          norm_first=True
+                                          )
+
         self.l2 = nn.Linear(2 * m * embedding_size, m * embedding_size)
-        self.l3 = nn.Linear(2 * m * embedding_size, m * embedding_size)
+        self.l3 = nn.Linear(m * embedding_size, m * embedding_size)
+
 
     def forward(self, prev_action, prev_state):
-        # prev_action = prev_action.float()
-        # prev_state = [prev_state[0].float(), prev_state[1].float()]
 
 
-        prev_action = torch.relu(self.l1(prev_action))
-        # print(prev_action.size(), prev_state[0].size(), prev_state[1].size())
-        # prev_action = torch.relu(self.batch_norm(prev_action))
-        # prev_state = (torch.relu(self.batch_norm(prev_state[0])), torch.relu(self.batch_norm(prev_state[1])))
+        prev_state = torch.relu(self.l2(prev_state))
 
-        output, ch = self.lstm_cell(prev_action, prev_state)
-        output = torch.relu(self.l2(output))
-        ch = torch.relu(self.l3(ch))
 
-        ch = torch.cat([output.unsqueeze(0).unsqueeze(0), ch.unsqueeze(0).unsqueeze(0)], dim=1)
+        prev_action = prev_action.squeeze()
+        batch = 20
+        if prev_state.shape[0] % 128 == 0 :
+            batch = 128
+        prev_action = prev_action.squeeze()
+        prev_action = prev_action.reshape(batch, prev_action.shape[0]//batch, prev_action.shape[1])
+        prev_state = prev_state.squeeze()
+        prev_state = prev_state.reshape(batch,prev_state.shape[0]//batch , prev_state.shape[1])
+
+        output = self.transformer(prev_action, prev_state)
+        output = output.reshape(batch * output.shape[1], output.shape[-1])
+
+        ch = torch.relu(self.l3(output))
+
+        output , ch = output.squeeze() , ch.squeeze()
 
         return output, ch
 
@@ -89,11 +101,6 @@ class Agent(nn.Module):
                 False)
         torch.nn.init.xavier_uniform_(self.relation_embedding.weight)
 
-        # self.relation_embedding = params['pretrained_embeddings_relation']
-
-        # self.train_entities = params['train_entity_embeddings']
-        # self.train_relations = params['train_relation_embeddings']
-
         self.num_rollouts = params['num_rollouts']
         self.test_rollouts = params['test_rollouts']
         self.LSTM_Layers = params['LSTM_layers']
@@ -124,6 +131,7 @@ class Agent(nn.Module):
         relation_embedding = self.relation_embedding(next_relations)
         entity_embedding = self.entity_embedding(next_entities)
 
+
         if self.use_entity_embeddings:
             action_embedding = torch.cat([relation_embedding, entity_embedding], dim=-1)
         else:
@@ -134,40 +142,17 @@ class Agent(nn.Module):
     def step(self, next_relations, next_entities, prev_state, prev_relation, query_embedding, current_entities,
               range_arr, first_step_of_test, entity_cluster_shared_informs):
 
-        # print(next_relations.size(), next_entities.size(), prev_state[0].size(), prev_relation.size(), query_embedding.size(), current_entities.size(),
-        #      label_action.size(), range_arr.size())
 
         prev_action_embedding = self.action_encoder(prev_relation, current_entities) # (original batch_size * num_rollout, 4*self.embedding_size)
 
-        prev_state = torch.unbind(prev_state, dim=1)
-        prev_state = [prev_state[0].squeeze(0), prev_state[1].squeeze(0)]
-
-        entity_cluster_shared_informs = torch.unbind(entity_cluster_shared_informs, dim=1)
-        entity_cluster_shared_informs = [entity_cluster_shared_informs[0].squeeze(0), entity_cluster_shared_informs[1].squeeze(0)]
-
-        # 1. one step of rnn
-
         new_prev_state = list()
 
-        # for i in range(2):
-        #     i_r, i_i, i_n = self.gate1_linear(prev_state[i]).chunk(3, 1)
-        #     h_r, h_i, h_n = self.gate2_linear(entity_cluster_shared_informs[i]).chunk(3, 1)
-        #
-        #     resetgate = torch.sigmoid(i_r + h_r)
-        #     inputgate = torch.sigmoid(i_i + h_i)
-        #     newgate = torch.tanh(i_n + (resetgate * h_n))
-        #
-        #     new_prev_state.append(newgate + inputgate * (entity_cluster_shared_informs[i] - newgate))
-
-        prev_state = (torch.cat([prev_state[0], entity_cluster_shared_informs[0]], dim=-1),
-                     torch.cat([prev_state[1], entity_cluster_shared_informs[1]], dim=-1))
-
-        # prev_state = (new_prev_state[0], new_prev_state[1])
+        prev_state = prev_state.squeeze()
+        prev_state = torch.cat((prev_state,entity_cluster_shared_informs), dim=-1)
 
         output, new_state = self.policy_step(prev_action_embedding, prev_state)
 
         # Get state vector
-        # prev_entity = self.entity_embedding[current_entities.cpu().numpy()]
         prev_entity = self.entity_embedding(current_entities)
         if self.use_entity_embeddings:
             state = torch.cat([output, prev_entity], dim=-1)
@@ -175,17 +160,12 @@ class Agent(nn.Module):
             state = output
 
         candidate_action_embeddings = self.action_encoder(next_relations, next_entities)
-        # query_embedding = self.relation_embedding[query_embedding.cpu().numpy()]
         query_embedding = self.relation_embedding(query_embedding)
 
         state_query_concat = torch.cat([state, query_embedding], dim=-1)
 
-        # MLP for policy#
-
         output = self.policy_mlp(state_query_concat)
-        # print(output.size())
         output_expanded = torch.unsqueeze(output, dim=1)  # [original batch_size * num_rollout, 1, 2D], D=self.hidden_size
-        # print(output_expanded.size(), candidate_action_embeddings.size())
         prelim_scores = torch.sum(candidate_action_embeddings * output_expanded, dim=2)
 
         # Masking PAD actions
@@ -200,7 +180,6 @@ class Agent(nn.Module):
         label_action = action.sample() # [original batch_size * num_rollout,]
 
         # loss
-        # 5a.
         loss = torch.nn.CrossEntropyLoss(reduce=False)(scores, label_action)
 
         # 6. Map back to true id
@@ -269,32 +248,8 @@ class ClusterAgent(nn.Module):
                      range_arr, first_step_of_test, entity_cluster_shared_informs):
 
         prev_action_embedding = self.cluster_action_encoder(prev_cluster, current_clusters)  # (original batch_size * num_rollout, 4*self.embedding_size)
-        
-        prev_state = torch.unbind(prev_state, dim=1)
-        prev_state = [prev_state[0].squeeze(0), prev_state[1].squeeze(0)]
 
-        entity_cluster_shared_informs = torch.unbind(entity_cluster_shared_informs, dim=1)
-        entity_cluster_shared_informs = [entity_cluster_shared_informs[0].squeeze(0), entity_cluster_shared_informs[1].squeeze(0)]
-
-        # print(entity_cluster_shared_informs[0].size(), prev_state[0].size())
-        # print(entity_cluster_shared_informs[1].size(), prev_state[1].size())
-        # print('')
-        # new_prev_state = list()
-        #
-        # for i in range(2):
-        #
-        #     i_r, i_i, i_n = self.gate1_linear(prev_state[i]).chunk(3, 1)
-        #     h_r, h_i, h_n = self.gate2_linear(entity_cluster_shared_informs[i]).chunk(3, 1)
-        #
-        #     resetgate = torch.sigmoid(i_r + h_r)
-        #     inputgate = torch.sigmoid(i_i + h_i)
-        #     newgate = torch.tanh(i_n + (resetgate * h_n))
-        #     # print(newgate.size(), inputgate.size(), entity_cluster_shared_informs[i].size(), newgate.size())
-        #     new_prev_state.append(newgate + inputgate * (entity_cluster_shared_informs[i] - newgate))
-
-        prev_state = (torch.cat([prev_state[0], entity_cluster_shared_informs[0]], dim=-1),
-                      torch.cat([prev_state[1], entity_cluster_shared_informs[1]], dim=-1))
-        # prev_state = (new_prev_state[0], new_prev_state[1])
+        prev_state = torch.cat((prev_state,entity_cluster_shared_informs), dim=-1)
 
         # 1. one step of rnn
         output, new_state = self.policy_step(prev_action_embedding, prev_state)
@@ -306,8 +261,6 @@ class ClusterAgent(nn.Module):
         else:
             state = output
 
-        # prev_cluster_emb = self.cluster_embedding(prev_cluster)
-
         candidate_action_embeddings = self.cluster_action_encoder(prev_possible_clusters, next_clusters)
 
         query_embedding = self.cluster_embedding(end_cluster)
@@ -315,12 +268,9 @@ class ClusterAgent(nn.Module):
 
         state_query_concat = torch.cat([state, query_embedding], dim=-1)
 
-        # MLP for policy#
 
         output = self.policy_mlp(state_query_concat)
-        # print(output.size())
         output_expanded = torch.unsqueeze(output, dim=1)  # [original batch_size * num_rollout, 1, 2D], D=self.hidden_size
-        # print(output_expanded.size(), candidate_action_embeddings.size())
         prelim_scores = torch.sum(candidate_action_embeddings * output_expanded, dim=2)
 
         # Masking PAD actions
@@ -336,14 +286,9 @@ class ClusterAgent(nn.Module):
         label_action = action.sample()  # [original batch_size * num_rollout,]
 
         # loss
-        # 5a.
         loss = torch.nn.CrossEntropyLoss(reduce=False)(scores, label_action)
 
         # 6. Map back to true id
         chosen_relation = next_clusters[list(torch.stack([range_arr, label_action]))]
 
         return loss, new_state, F.log_softmax(scores), label_action, chosen_relation, F.log_softmax(scores)
-
-
-
-
